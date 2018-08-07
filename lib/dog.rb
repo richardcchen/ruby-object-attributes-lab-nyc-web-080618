904bec0ce6080698118f60e6a0ee5db9ff2e87da
Richard Chen <richard.c.chen@gmail.com>
2018-08-07 14:34:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  def initializename(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+
+    def initializebreed(breed)
+    @breed = breed 
+  end
+ 
+  def breed
+    @breed
+  end
+ 
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
